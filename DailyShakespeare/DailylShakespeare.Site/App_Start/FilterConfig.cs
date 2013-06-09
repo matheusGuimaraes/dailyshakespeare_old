d8e28177b849614e02b293f1deda1dc0918f45db
Matheus Guimaraes <myemail.matheus@gmail.com>
2013-06-09 11:33:15 +0100

    removed error filter

--- DailyShakespeare/DailylShakespeare.Site/App_Start/FilterConfig.cs
+++ DailyShakespeare/DailylShakespeare.Site/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace DailylShakespeare.Site
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            //filters.Add(new HandleErrorAttribute());
         }
     }
 }
